8ee905df1789b8c06a44d2812321338887b66601
Wout Bittremieux <wout@bittremieux.be>
2016-12-30 18:52:35 +0100

    Don't do repeated string concatenation

--- tweet.py
+++ tweet.py
@@ -18,18 +18,14 @@ def file_len(fname):
     return i + 1
 
 # Count the amount of lines in the old and new lactos files
-oldlen = file_len("/home/pi/lactobot/ncbi_files/lactos_old.txt")
-newlen = file_len("/home/pi/lactobot/ncbi_files/lactos.txt")
-diflen = newlen - oldlen
-
-# Convert integers to strings
-newlen_str = str(newlen)
-diflen_str = str(diflen)
+num_lactos_old = file_len("/home/pi/lactobot/ncbi_files/lactos_old.txt")
+num_lactos_new = file_len("/home/pi/lactobot/ncbi_files/lactos.txt")
+num_lactos_dif = num_lactos_new - num_lactos_old
 
 # Add date and count to track file
 date = time.strftime("%Y%m%d")
 with open("track_count.txt", "a") as myfile:
-    myfile.write(date + "\t" + newlen_str + "\n")
+    myfile.write("{}\t{}\n".format(date, num_lactos_new))
 
 # Read in track_count file for plot
 track_count = pd.read_table("/home/pi/lactobot/track_count.txt", sep="\t")
@@ -52,11 +48,11 @@ api = Twython(apiKey, apiSecret, accessToken, accessTokenSecret)
 
 # Set up photo and tweet
 photo = open("image.png", "rb")
-tweetStr = "There are currently " + newlen_str + " Lactobacillus assemblies available. That's " + diflen_str + " more than yesterday. #lactobot"
+tweetStr = "There are currently {} Lactobacillus assemblies available. That's {} more than yesterday. #lactobot".format(num_lactos_new, num_lactos_dif)
 
 # Update status only when diflen differs from 0
-if diflen != 0:
-    api.update_status_with_media(media=photo,status=tweetStr)
-    print("Tweeted: " + tweetStr)
+if num_lactos_dif != 0:
+    api.update_status_with_media(media=photo, status=tweetStr)
+    print("Tweeted: {}".format(tweetStr))
 else:
     print("I did not tweet today, because nothing interesting happened")
